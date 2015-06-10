bd24e9465395b8788014ebc0ce094a2ca73b672c
Mykhaylo Skubenych <mykhaylo.skubenych@gmail.com>
2015-06-10 13:42:32 +0300

    Articles.

--- wp-content/themes/metadigital/index.php
+++ wp-content/themes/metadigital/index.php
@@ -28,6 +28,63 @@
           <source src="./wp-content/themes/metadigital/video/metaspace.mp4" type="video/mp4" codecs="avc1.42E01E, mp4a.40.2">
       </video>
     </div>
+    <div class="articles">
+      <div class="category">
+        <div class="item">
+          <span class="icon">
+            <img src="./wp-content/themes/metadigital/images/marketing-icon.png"">
+          </span>
+          <br>
+          ИНТЕРНЕТ-МАРКЕТИНГ
+        </div>
+      </div>
+      <div class="category">
+        <div class="item">
+          <span class="icon">
+            <img src="./wp-content/themes/metadigital/images/device-icon.png"">
+          </span>
+          <br>
+          ДЕВАЙС?
+        </div>
+      </div>
+      <div class="category">
+        <div class="item">
+          <span class="icon">
+            <img src="./wp-content/themes/metadigital/images/design-icon.png"">
+          </span>
+          <br>
+          WEB-ДИЗАЙН
+        </div>
+      </div>
+      <div class="category">
+        <div class="item">
+          <span class="icon">
+            <img src="./wp-content/themes/metadigital/images/bitrix-icon.png"">
+          </span>
+          <br>
+          ПРОДУКТЫ 1C БИТРИКС
+        </div>
+      </div>
+      <div class="category">
+        <div class="item">
+          <span class="icon">
+            <img src="./wp-content/themes/metadigital/images/applications-icon.png"">
+          </span>
+          <br>
+          РАЗРАБОТКА ПРИЛОЖЕНИЙ
+        </div>
+      </div>
+      <div class="category">
+        <div class="item">
+          <span class="icon">
+            <img src="./wp-content/themes/metadigital/images/outstuffing-icon.png"">
+          </span>
+          <br>
+          АУСТАФФИНГ
+        </div>
+      </div>
+      <div style="clear: left"></div>
+    </div>
     <?php wp_footer(); ?>
   </body>
 </html>
