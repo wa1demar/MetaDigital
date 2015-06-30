23b9b02adb94853fa6cb76e71f5f901a01feac02
Mykhaylo Skubenych <mykhaylo.skubenych@gmail.com>
2015-06-30 17:12:32 +0300

    Fix onclick.

--- wp-content/themes/metadigital/js/zoom_map.js
+++ wp-content/themes/metadigital/js/zoom_map.js
@@ -2,19 +2,21 @@
 
     window.$ = jQuery;
 
-    $('.main-footer').click(function () {
-        var $this = $(this);
-        var mapId = parseInt($this.attr("data-map"));
+    $(document).ready(function(){
+        $('.main-footer').click(function () {
+            var $this = $(this);
+            var mapId = parseInt($this.attr("data-map"));
 
-        switch (mapId) {
-            case 0:
-                zoomImage($this, "./wp-content/themes/metadigital/images/map2.png", mapId);
-                break;
-            case 1:
-                zoomImage($this, "./wp-content/themes/metadigital/images/map3.png", mapId);
-                break;
+            switch (mapId) {
+                case 0:
+                    zoomImage($this, "./wp-content/themes/metadigital/images/map2.png", mapId);
+                    break;
+                case 1:
+                    zoomImage($this, "./wp-content/themes/metadigital/images/map3.png", mapId);
+                    break;
 
-        }
+            }
+        });
     });
 
     var zoomImage = function (el, background_img, mapId) {
