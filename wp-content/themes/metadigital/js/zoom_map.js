bb85dec347be48cb307e3cc48e6f8961f850ca2d
waldemar <zehelloworld@gmail.com>
2015-07-01 12:27:02 +0300

    zoom

--- wp-content/themes/metadigital/js/zoom_map.js
+++ wp-content/themes/metadigital/js/zoom_map.js
@@ -3,6 +3,12 @@
     window.$ = jQuery;
 
     $(document).ready(function () {
+
+        preload([
+            "./wp-content/themes/metadigital/images/map2.png",
+            "./wp-content/themes/metadigital/images/map3.png"
+        ]);
+
         $('.main-footer').click(function () {
             var $this = $(this);
             var mapId = parseInt($this.attr("data-map"));
@@ -20,10 +26,18 @@
     });
 
     var zoomImage = function (el, background_img, mapId) {
+            el.attr("data-map", (++mapId));
+            el.css({'background-image': 'url(' + background_img + ')'});
 
-        el.attr("data-map", (++mapId));
+    };
 
-        el.css({'background-image': 'url(' + background_img + ')'})
+    function preload(arg) {
+        pics = [];
+        for (var i = 0; i < arg.length; i++) {
+            console.log(arg[i]);
+            pics[i] = new Image();
+            pics[i].src = arg[i];
 
+        }
     }
 })(jQuery);
