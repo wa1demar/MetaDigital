7279f38727500cc900793cbbde68d007cb9dccc6
waldemar <zehelloworld@gmail.com>
2015-07-04 16:59:33 +0300

    tooltip style

--- wp-content/themes/metadigital/functions.php
+++ wp-content/themes/metadigital/functions.php
@@ -5,7 +5,6 @@
 
 require_once 'functions/init.php';
 require_once 'functions/services_post_type.php';
-require_once 'functions/gallery_post_type.php';
 require_once 'functions/siteinfo_post_type.php';
 require_once 'functions/wdm_gallery2_post_type.php';
 
