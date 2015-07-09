9d9decf2e97dec392ef4c47e671565a64a6b9e03
waldemar <zehelloworld@gmail.com>
2015-07-09 20:28:43 +0300

    link

--- wp-content/themes/metadigital/js/mainController.js
+++ wp-content/themes/metadigital/js/mainController.js
@@ -102,8 +102,8 @@
                         window.locale = lang;
                         $scope.categories = data;
 
-                        if ($.urlParam('service')) {
-                            var result = $scope.findCategory($scope.categories, $.urlParam('service'));
+                        if (window.location.hash.substr(1)) {
+                            var result = $scope.findCategory($scope.categories, window.location.hash.substr(1));
                             console.log(result);
                             if (result == null) {
                                 $scope.current_category = $scope.categories[0];
