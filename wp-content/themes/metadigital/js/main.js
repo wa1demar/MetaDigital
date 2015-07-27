31f922bf006ed918466ea4795e456b301fea7f44
waldemar <zehelloworld@gmail.com>
2015-07-27 14:19:53 +0300

    tap

--- wp-content/themes/metadigital/js/main.js
+++ wp-content/themes/metadigital/js/main.js
@@ -352,12 +352,12 @@
 
                 }
 
-                    $("#languagebox .locale-ru").on("tap click touchstart",function(){
+                    $("#languagebox .locale-ru").bind("tap click touchstart",function(){
                         choose('ru');
                         return false;
                     });
 
-                    $("#languagebox .locale-en").on("tap click touchstart",function(){
+                    $("#languagebox .locale-en").bind("tap click touchstart",function(){
                         choose('en');
 
                         return false;
